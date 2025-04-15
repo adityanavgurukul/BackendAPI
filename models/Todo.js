ffa0a8ec84bbb346fd52ac10c99c62722c086c7b
adityanavgurukul <163106361+adityanavgurukul@users.noreply.github.com>
2025-04-15 11:43:06 +0530

    Update Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -14,11 +14,6 @@ const todoSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Todo', todoSchema);
